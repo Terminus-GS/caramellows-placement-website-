fe02322670d5b38ac7f4f27702009266f3f9c22b
Terminus-GS <georgedavisps@gmail.com>
2016-10-17 21:41:54 +0100

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,98 @@
+<?php
+include('header.php');
+?>
+
+    <body>
+        <!-- <div class="overlay"></div>
+    <section id="page-content">
+        <div class="row mainslider" style="
+    width: 80%;
+"> -->
+        <div class="row"  style="padding-bottom: 20px; padding-top: 20px;">
+            <div class="small-8 columns orbitClass" style="margin: 0 auto;
+    display: block; float: none;">
+                <div class="orbit" role="region" aria-label="Example Candy Pictures" data-orbit>
+                    <ul class="orbit-container">
+                        <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+                        <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+                        <li class="is-active orbit-slide"> <img class="orbit-image" src="images/trufffles.jpg" alt="Space">
+                            <figcaption class="orbit-caption">Truffles</figcaption>
+                        </li>
+                        <li class="orbit-slide"> <img class="orbit-image" src="images/smores.jpg" alt="Space">
+                            <figcaption class="orbit-caption">Smores</figcaption>
+                        </li>
+                        <li class="orbit-slide"> <img class="orbit-image" src="images/brownies.jpg" alt="Space">
+                            <figcaption class="orbit-caption">Brownies</figcaption>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        </section>
+        <section>
+            <div class="row">
+                <div class="small-12 columns AboutInfo">
+                    <h6 class="AboutInfoTitle">A Little About Caramellows</h6>
+                    <p class="AboutInfoPara">At Caramellows Candy, we sell a wide variety of handmade sweets, treats and desserts for all events and occasions. From truffles to brownies, Peanut Butter Treats to different sorts of fudge</p>
+                    <p class="AboutInfoPara">We are very proud to hold a 5-hygiene rating, and while being a small business, everything we do is professional.</p>
+                    <p class="AboutInfoPara"> You will find us every sunday at the Sprowston Park & Ride Car Boot, as well as the odd farmers market and carboot in and around Norwich</p>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="row">
+                <div class="small-12 columns Reviews">
+                    <h6 class="ReviewTitle">Reviews</h6>
+                    <p class="Review1"> 'Omg you really need to open a shop the marshmallow pops were delicious also one of the best popcorns I've ever tasted all packaged lovely very reasonable prices very friendly service I will definitely order again and again and again'
+                        <br>Joanna Spilling, 5-Stars </p>
+                    <p class="Review2"> 'I bought a candy cup for my daughters birthday as she doesn't like cake. She absolutely loved it and Rebecca made it look all pretty and the cup was completely full. So happy with your service, definitely buy again.
+                        <br> Louise Palmer, 5-Stars</p>
+                    <p class="Review3">'Wow! My absolute favourite was the popcorn one (can't remember the name of it) but the whole bag was demolished in no time whatsoever! Absolutely amazing!!!!'
+                        <br> Strawberry cheesecake truffles, i don't normally like strawberry cheesecake but my gosh these truffles were delicious! Last but not least, the brownie the BEST!!!!!'
+                        <br>Kate Applegate, 5-Stars</p>
+                </div>
+            </div>
+        </section>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/vendor/what-input.min.js"></script>
+        <script src="js/vendor/foundation.min.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
+    <footer>
+        <div class="row">
+            <div class="large-4 columns footerleft">
+                <h5 class="FLtitle">Where Are We?</h5>
+                <ul class="FLlist" style="">
+                    <li>2 Newbegin Close</li>
+                    <li>NR1 4PU</li>
+                    <li>Norwich</li>
+                    <li>Norfolk</li>
+                </ul>
+            </div>
+            <div class="large-4 columns footermiddle">
+                <h5 class="FLtitle">Quick-Contact</h5>
+                <ul class="FMlist" style="">
+                    <li>Email Address: caramellowscandy@gmail.com</li>
+                    <li>Phone: 07936854224</li>
+                    <li>
+                        <a href="https://www.facebook.com/CarramelowsCandy/?fref=ts" target="_blank"> <i class="fi-social-facebook"></i></li>
+                    </a>
+                </ul>
+            </div>
+            <div class="large-4 columns footerright">
+                <h5 class="FLtitle">Customer Care &amp; Policies</h5>
+                <ul class="FRlist" style="">
+                    <li>FAQS</li>
+                    <li>Returns</li>
+                    <li>Allergies</li>
+                    <li>Contact Us</li>
+                    <li>Terms &amp; Conditions</li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    </html>
---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en" dir="ltr">
+
+<head>
+    <meta charset="UTF-8" />
+    <title>Caramellows</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/foundation.min.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="CCcss.css" />
+    <link rel="stylesheet" href="foundation-icons/foundation-icons.css" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Fira+Sans"> 
+    <script type="text/javascript" src="jquery-3.1.0.min.js"></script>
+    </head>
+<header id="page-header">
+    
+    <div class="row">
+        <div class="small-12 columns">
+            <img class="ccimg" img src="images/caramellows.png" alt="Caramellows Candy">
+            <div class="title-bar" data-responsive-toggle="example-menu" data-hide-for="medium">
+                <button class="menu-icon dark" type="button" onclick="myFunction()" data-toggle="menu-fade" aria-controls="menu-fade"></button>            
+            </div>
+            <div class="top-bar" id="menu-fade" data-animate="fade-in fade-out" data-toggler data-animate="fade-in fade-out" aria-expanded="false">
+                <div class="top-bar-left">
+                    <ul class="dropdown menu" data-dropdown-menu>
+                        <ul class="vertical medium-horizontal menu">
+                             <li class="is-dropdown-submenu-parent">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Shop</a></li>
+                            <li><a href="CCAbout.html">About</a></li>
+                            <li><a href="#">Review</a></li>
+                            <li><a href="#">Contact</a></li>
+                            </li>
+                        </ul>                       
+                    </ul>
+                </div>
+            </div> 
+        </div>
+    </div>
+<!--<script type="text/javascript">
+    $(".menu-icon").click(function () {
+    $(".overlay").show();
+});
+</script>-->
+
+</header>
